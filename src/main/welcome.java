7d9d716e3eda5403267df28dca4c1d2cad4b704d
Yash Kumar <w0749495@myscc.ca>
2020-11-12 00:40:39 -0500

    Work done for the week and javafx files working properly

--- src/main/welcome.java
+++ src/main/welcome.java
@@ -1,21 +1,7 @@
 package main;
-
 import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.*;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontPosture;
-import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 import scene.mainScene;
-
-import java.io.FileInputStream;
 
 public class welcome extends Application {
      public static Stage stage;
