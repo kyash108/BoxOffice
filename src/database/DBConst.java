d50e7b37568a0d3c1b8dfa7785bd1e4b2b909cfb
Sidharth Bajaj <w0744581@myscc.ca>
2020-11-17 03:05:07 -0500

    added strings related to CRUD and initialized table commands along with create strings for insert,update and delete commands

---
+++ src/database/DBConst.java
@@ -0,0 +1,119 @@
+package database;
+
+public class DBConst {
+
+
+    // SPORTS TABLE
+    public static final String TABLE_SPORTS = "sports";
+    public static final String SPORTS_COLUMN_ID = "id";
+    public static final String SPORTS_COLUMN_TITLE = "title";
+    public static final String SPORTS_COLUMN_RDATE = "date";
+    public static final String SPORTS_COLUMN_DIRECTOR = "director";
+    public static final String SPORTS_COLUMN_BUDGET = "budget";
+
+    // COMEDY TABLE
+    public static final String TABLE_COMEDY = "comedy";
+    public static final String COMEDY_COLUMN_ID = "id";
+    public static final String COMEDY_COLUMN_TITLE = "title";
+    public static final String COMEDY_COLUMN_RDATE = "date";
+    public static final String COMEDY_COLUMN_DIRECTOR = "director";
+    public static final String COMEDY_COLUMN_BUDGET = "budget";
+
+    // ACTION SCI-FI
+    public static final String TABLE_SCI = "scifi";
+    public static final String SCI_COLUMN_ID = "id";
+    public static final String SCI_COLUMN_TITLE = "title";
+    public static final String SCI_COLUMN_RDATE = "date";
+    public static final String SCI_COLUMN_DIRECTOR = "director";
+    public static final String SCI_COLUMN_BUDGET = "budget";
+
+
+    //CRUD
+
+    // Create table
+    public static final String CREATE_TABLE_COMEDY =
+            " CREATE TABLE " + TABLE_COMEDY + "(" +
+                    COMEDY_COLUMN_ID + "int NOT NULL AUTO_INCREMENT, "+
+                    COMEDY_COLUMN_TITLE + "VARCHAR(50), "+
+                    COMEDY_COLUMN_RDATE + "DATE,"+
+                    COMEDY_COLUMN_DIRECTOR + "VARCHAR(70), "+
+                    COMEDY_COLUMN_BUDGET + "VARCHAR(40) ,"+
+                    "PRIMARY KEY("+COMEDY_COLUMN_ID+")"+
+                    ")";
+
+    public static final String CREATE_TABLE_SPORTS =
+            " CREATE TABLE " + TABLE_SPORTS + "(" +
+                    SPORTS_COLUMN_ID + "int NOT NULL AUTO_INCREMENT, "+
+                    SPORTS_COLUMN_TITLE + "VARCHAR(50), "+
+                    SPORTS_COLUMN_RDATE + "DATE,"+
+                    SPORTS_COLUMN_DIRECTOR + "VARCHAR(70), "+
+                    SPORTS_COLUMN_BUDGET + "VARCHAR(40) ,"+
+                    "PRIMARY KEY("+SPORTS_COLUMN_ID+")"+
+                    ")";
+
+    public static final String CREATE_TABLE_SCIFI =
+            " CREATE TABLE " + TABLE_SCI + "(" +
+                    SCI_COLUMN_ID + "int NOT NULL AUTO_INCREMENT, "+
+                    SCI_COLUMN_TITLE + "VARCHAR(50), "+
+                    SCI_COLUMN_RDATE + "DATE,"+
+                    SCI_COLUMN_DIRECTOR + "VARCHAR(70), "+
+                    SCI_COLUMN_BUDGET + "VARCHAR(40) ,"+
+                    "PRIMARY KEY("+SCI_COLUMN_ID+")"+
+                    ")";
+
+    //Delete statements for tables
+
+    public static final String DELETE_DATA_COMEDY =
+        "DELETE FROM " + TABLE_COMEDY +
+            "WHERE " + COMEDY_COLUMN_ID ;
+
+    public static final String DELETE_DATA_SCI =
+            "DELETE FROM " + TABLE_SCI +
+                    "WHERE " + SPORTS_COLUMN_ID ;
+
+    public static final String DELETE_DATA_SPORTS =
+            "DELETE FROM " + TABLE_SPORTS +
+                    "WHERE " + SPORTS_COLUMN_ID ;
+
+
+
+    // update statements for tables
+
+    public static final String UPDATE_SCI =
+            "UPDATE "+TABLE_SCI+ " SET " +
+                    SCI_COLUMN_TITLE + " " + SCI_COLUMN_DIRECTOR + " " + SCI_COLUMN_RDATE +
+                    " " + SCI_COLUMN_BUDGET + " " + " WHERE " + SCI_COLUMN_ID + " ";
+
+    public static final String UPDATE_COMEDY =
+            "UPDATE "+ TABLE_COMEDY+ " SET "+
+                    COMEDY_COLUMN_TITLE + " " + COMEDY_COLUMN_DIRECTOR + " " + COMEDY_COLUMN_RDATE +
+                    " " + COMEDY_COLUMN_BUDGET + " " + " WHERE " + SCI_COLUMN_ID + " ";
+
+    public static final String UPDATE_SPORTS =
+            "UPDATE "+ TABLE_SPORTS+ " SET "+
+                    SPORTS_COLUMN_TITLE + " , " + SPORTS_COLUMN_DIRECTOR + " , " + SPORTS_COLUMN_RDATE +
+                    " , " + SPORTS_COLUMN_BUDGET + " , " + " WHERE " + SPORTS_COLUMN_ID + " ";
+
+
+    // insert statements for tables
+    public static final String INSERT_SCI =
+            "INSERT INTO " + TABLE_SCI + "(" +
+                    SCI_COLUMN_TITLE + "," + SCI_COLUMN_DIRECTOR + "," + SCI_COLUMN_RDATE+
+                    ","+SCI_COLUMN_BUDGET+") VALUES ()"
+                    //"('"+")" yet to get values from the user
+     ;
+
+    public static final String INSERT_COMDEY =
+            "INSERT INTO " + TABLE_COMEDY + "(" +
+                    COMEDY_COLUMN_TITLE + "," + COMEDY_COLUMN_DIRECTOR + "," + COMEDY_COLUMN_RDATE+
+                    ","+COMEDY_COLUMN_BUDGET+") VALUES ()"
+            //"('"+")" yet to get values from the user
+            ;
+
+    public static final String INSERT_SPORTS =
+            "INSERT INTO " + TABLE_SPORTS + "(" +
+                    SPORTS_COLUMN_TITLE + "," + SPORTS_COLUMN_DIRECTOR + "," + SPORTS_COLUMN_RDATE+
+                    ","+SPORTS_COLUMN_BUDGET+") VALUES ()"
+            //"('"+")" yet to get values from the user
+            ;
+}
--- src/database/Database.java
+++ src/database/Database.java
@@ -1,19 +1,28 @@
 package database;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.*;
 
 public class Database {
     public static Database instance = null;
+    private Connection connection = null;
 
+    //
     private Database(){
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            Connection connection = DriverManager.getConnection("jdbc:mysql://php.scweb.ca"+Login.DB_NAME+"?useSSL=false",
+            connection = DriverManager.getConnection("jdbc:mysql://php.scweb.ca"+Login.DB_NAME+"?useSSL=false",
                     Login.DB_USER, Login.DB_PASSWORD);
             System.out.println("connection created");
 
         } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // database
+        try {
+            createTable(DBConst.TABLE_COMEDY,DBConst.CREATE_TABLE_COMEDY,connection);
+            createTable(DBConst.TABLE_SCI,DBConst.CREATE_TABLE_SCIFI,connection);
+            createTable(DBConst.TABLE_SPORTS,DBConst.CREATE_TABLE_SPORTS,connection);
+        }catch (Exception e){
             e.printStackTrace();
         }
     }
@@ -22,6 +31,33 @@ public class Database {
             instance = new Database();
         }
         return instance;
+    }
+
+    public Connection getConnection(){
+        return connection;
+    }
+    public void close(){
+        System.out.println("Closing Application");
+        try {
+            connection.close();
+        }catch (SQLException e){
+            connection = null;
+            e.printStackTrace();
+        }
+    }
+
+    public void createTable(String tableName, String tableQuery, Connection connection) throws SQLException {
+        Statement createComedyTable;
+        DatabaseMetaData metaData = connection.getMetaData();
+        ResultSet resultSet = metaData.getTables(null,null,tableName,null);
+        if (resultSet.next()){
+            System.out.println(tableName+" Table already exists!");
+        }else {
+            createComedyTable = connection.createStatement();
+            createComedyTable.execute(tableQuery);
+            System.out.println(" The " + tableName + " table has been inserted");
+        }
+
     }
 }
 
