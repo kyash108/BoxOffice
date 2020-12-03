00d2c59139214d0bd5b409536b5055d0e1df6579
Yash Kumar <w0749495@myscc.ca>
2020-12-02 20:12:11 -0500

    working on gitignore

--- src/database/Login.java
+++
@@ -1,8 +0,0 @@
-package database;
-
-public class Login {
-
-    public static final String DB_NAME = "sbajajdb";
-    public static final String DB_USER = "sbajaj";
-    public static final String DB_PASSWORD = "znj12znj12ddmz7ddmz7";
-}
